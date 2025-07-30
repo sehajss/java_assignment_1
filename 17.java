f7741eb762c9dae727a40dc6022f160b6cd17fe3
sehajss <sehajsinghsidhu@gmail.com>
2025-07-30 22:52:02 +0530

    Create 17.java

---
+++ 17.java
@@ -0,0 +1,8 @@
+public class Identifiers {
+    public static void main(String[] args) {
+        int valid_name = 10;
+        // int 1invalid = 20; // invalid
+        int $ok = 5;
+        System.out.println(valid_name + $ok);
+    }
+}
