d8248f7994570095830fd87770f780a9eb09c793
sehajss <sehajsinghsidhu@gmail.com>
2025-07-30 23:39:39 +0530

    47.java

---
+++ 47.java
@@ -0,0 +1,8 @@
+public class ReverseStr {
+    public static void main(String[] args) {
+        String s = "hello", rev = "";
+        for (int i = s.length()-1; i >= 0; i--)
+            rev += s.charAt(i);
+        System.out.println(rev);
+    }
+}
