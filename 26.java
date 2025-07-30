5faf49319bff57481d76ab0aa20398155ac08b2b
sehajss <sehajsinghsidhu@gmail.com>
2025-07-30 22:57:25 +0530

    Create 26.java

---
+++ 26.java
@@ -0,0 +1,7 @@
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        for (int i = arr.length - 1; i >= 0; i--)
+            System.out.print(arr[i] + " ");
+    }
+}
