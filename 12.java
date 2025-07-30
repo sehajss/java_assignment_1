817577d2c322d06241eb2e6263d5859ef44e57f1
sehajss <sehajsinghsidhu@gmail.com>
2025-07-30 22:50:31 +0530

    Create 12.java

---
+++ 12.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = "Java";
+        str.concat(" Programming");
+        System.out.println(str);  // Still prints "Java"
+    }
+}
