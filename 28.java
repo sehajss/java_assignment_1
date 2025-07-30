d0a0355335ee599e188f274ce15f8d2567f86675
sehajss <sehajsinghsidhu@gmail.com>
2025-07-30 22:57:54 +0530

    Create 28.java

---
+++ 28.java
@@ -0,0 +1,13 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 2, 4, 1};
+        for (int i = 0; i < arr.length-1; i++)
+            for (int j = 0; j < arr.length-i-1; j++)
+                if (arr[j] > arr[j+1]) {
+                    int t = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = t;
+                }
+        for (int n : arr) System.out.print(n + " ");
+    }
+}
