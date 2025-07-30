5d00dc65945fdfc9b2f5d994b6304da4a38f8f55
sehajss <sehajsinghsidhu@gmail.com>
2025-07-30 22:59:00 +0530

    Create 32.java

---
+++ 32.java
@@ -0,0 +1,8 @@
+public class Operators {
+    public static void main(String[] args) {
+        int a = 5, b = 2;
+        System.out.println(a + b); // arithmetic
+        System.out.println(a > b); // relational
+        System.out.println(a > 0 && b > 0); // logical
+    }
+}
