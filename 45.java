8b9191645628e07a25524febe70ae51d9d48ceeb
sehajss <sehajsinghsidhu@gmail.com>
2025-07-30 23:37:35 +0530

    45.java

---
+++ 45.java
@@ -0,0 +1,10 @@
+final class BaseFinal {
+    final void display() {
+        System.out.println("Final Method in Final Class");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        new BaseFinal().display();
+    }
+}
