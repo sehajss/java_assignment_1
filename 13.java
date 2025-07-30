3c1ca6ead8f5ab5f2dcf57d72d435def86106247
sehajss <sehajsinghsidhu@gmail.com>
2025-07-30 22:50:46 +0530

    Create 13.java

---
+++ 13.java
@@ -0,0 +1,15 @@
+public class DataTypes {
+    byte b;
+    short s;
+    int i;
+    long l;
+    float f;
+    double d;
+    char c;
+    boolean bool;
+
+    public static void main(String[] args) {
+        DataTypes dt = new DataTypes();
+        System.out.println(dt.b + " " + dt.s + " " + dt.i + " " + dt.l);
+    }
+}
