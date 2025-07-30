777e5ae459d57ab541d7a67d300b1f878f1a5643
sehajss <sehajsinghsidhu@gmail.com>
2025-07-30 23:00:06 +0530

    Create 34.java

---
+++ 34.java
@@ -0,0 +1,7 @@
+public class Ternary {
+    public static void main(String[] args) {
+        int a = 10;
+        String msg = (a % 2 == 0) ? "Even" : "Odd";
+        System.out.println(msg);
+    }
+}
